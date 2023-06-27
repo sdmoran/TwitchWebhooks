c921de5b10bef21f89bcc005953cdf76f2746d85
Sam Moran <smoran@wpi.edu>
2023-06-26 19:28:41 -0700

    Update AuthView test to user router.state instead of window

--- twitch-frontend/src/components/AuthView.Test.tsx
+++ twitch-frontend/src/components/AuthView.Test.tsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from '@testing-library/react'
 import { createMemoryRouter, RouterProvider } from 'react-router-dom'
 import routeConfig from '../routeConfig'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 
 const routes = routeConfig(false)
 
@@ -9,11 +10,15 @@ test('Renders auth page', async () => {
   const router = createMemoryRouter(routes, {
     initialEntries: ['/auth']
   })
-  render(
-    <RouterProvider router={router} />
-  )
 
-  expect(window.location.pathname).toEqual('/auth')
+  act(() => {
+    render(
+      <RouterProvider router={router} />
+    )
+  })
+
   const msg = await waitFor(() => screen.getByText('Authorize Application'))
+  console.log(router.state.location.pathname)
+  expect(router.state.location.pathname).toEqual('/auth')
   expect(msg).toBeInTheDocument()
 })
