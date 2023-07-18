e8b3a8cb3bdc630bf46b25197871fe0ef053d231
Sam Moran <smoran@wpi.edu>
2023-07-18 15:56:55 -0700

    Update notificationview test

--- twitch-frontend/src/components/NotificationView.test.tsx
+++ twitch-frontend/src/components/NotificationView.test.tsx
@@ -45,7 +45,7 @@ test('Shows error when no subscriptions are passed in URL parameters', async () 
         <RouterProvider router={router} />
   )
 
-  const msg = await screen.findByText('UNKNOWN EVENT TYPE')
+  const msg = await screen.findByText('An error occurred: No subscriptions selected!')
   expect(msg).toBeInTheDocument()
 })
 
@@ -57,9 +57,9 @@ test('Renders NotificationView with a valid Twitch User ID and subscription type
         <RouterProvider router={router} />
   )
 
-  const msg = await screen.findByText('UNKNOWN EVENT TYPE')
+  const msg = await screen.findByText('Thanks for following, Sample User!')
   expect(msg).toBeInTheDocument()
-  const testMsg = screen.getByTestId('notification-elt') // notification hidden if showTest not in query params
+  const testMsg = screen.getByTestId('notification-elt')
   expect(testMsg).toBeInTheDocument()
 })
 
